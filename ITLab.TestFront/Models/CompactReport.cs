70aeedec1c39557bd6d12971c8ee25e899c33cb8
Maksim Makushchenko <maksalmak@gmail.com>
2020-04-14 02:28:17 +0300

    Add project files.

---
+++ ITLab.TestFront/Models/CompactReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab.TestFront.Models
+{
+    public class CompactReport
+    {
+        public string Id { get; set; }
+        public Guid ReportSender { get; set; }
+        public string ReportSenderName { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+        public bool Archived { get; set; }
+    }
+}
---
+++ ITLab.TestFront/Models/CreateReportRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab.TestFront.Models
+{
+    public class CreateReportRequest
+    {
+        [Required]
+        public string Text { get; set; }
+    }
+}
