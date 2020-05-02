aa7534028b69a1457a39d0dfa8afed359bcd1bfa
Maksim Makushchenko <maksalmak@gmail.com>
2020-05-02 11:30:42 +0300

    Time range for admin, repair files page

---
+++ ITLab.TestFront/RemoteApi/IFilesApi.cs
@@ -0,0 +1,16 @@
+﻿using ITLab.TestFront.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab.TestFront.RemoteApi
+{
+    [Headers("Authorization: Bearer")]
+    public interface IFilesApi
+    {
+        [Get("/files")]
+        Task<List<FileObjectModel>> FilesList(string user);
+    }
+}
